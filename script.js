ab05aade608de7717933881c83908d26afb61a48
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-08 14:51:10 +0300

    add prototype

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function Details(size, crust, toppings, quantity) {
     this.selectedToppings = toppings;
     this.selectedQuantity = quantity;
 }
+
+function Total(sizePrice, quantity, delivery) {
+    this.sizePrice = sizePrice;
+    this.quantity = quantity;
+    this.delivery = delivery;
+}
+Total.prototype.finalTotal = function() {
+    return this.sizePrice * this.quantity + this.delivery;
+}
+
+$(document).ready(function() {
+            $('.allme').submit(function(event) {
+                        event.preventDefault();
+                        var sizePrice = {
+                            small: 650,
+                            medium: 850,
+                            large: 1250
+                        };
