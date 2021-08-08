125fea89594451d84623588f10f12234eddcd8f7
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-08 15:26:33 +0300

    add prompt button

--- script.js
+++ script.js
@@ -15,74 +15,93 @@ Total.prototype.finalTotal = function() {
 }
 
 $(document).ready(function() {
-            $('.allme').submit(function(event) {
-                        event.preventDefault();
-                        var sizePrice = {
-                            small: 650,
-                            medium: 850,
-                            large: 1250
-                        };
-                        var toppingPrice = [{
-                            pepperoni: {
-                                price: 250,
-                            },
-                            sausage: {
-                                price: 150,
-                            },
-                            bacon: {
-                                price: 200
-                            }
-                        }]
-                        var crustPrice = {
-                            crispy: 300,
-                            stuffed: 200,
-                            gluten: 100
-                        };
+    $('.allme').submit(function(event) {
+        event.preventDefault();
+        var sizePrice = {
+            small: 650,
+            medium: 850,
+            large: 1250
+        };
+        var toppingPrice = [{
+            pepperoni: {
+                price: 250,
+            },
+            sausage: {
+                price: 150,
+            },
+            bacon: {
+                price: 200
+            }
+        }]
+        var crustPrice = {
+            crispy: 300,
+            stuffed: 200,
+            gluten: 100
+        };
 
 
-                        function sizeCalcPrice(size) {
-                            if (size === "small") {
-                                return sizePrice.small * 1;
-                            } else if (size === "medium") {
-                                return sizePrice.medium * 1;
-                            } else {
-                                return sizePrice.large * 1;
-                            }
-                        }
+        function sizeCalcPrice(size) {
+            if (size === "small") {
+                return sizePrice.small * 1;
+            } else if (size === "medium") {
+                return sizePrice.medium * 1;
+            } else {
+                return sizePrice.large * 1;
+            }
+        }
 
-                        function crustCalcPrice(crust) {
-                            if (crust === "crispy") {
-                                return crustPrice.crispy * 1;
-                            } else if (crust === "stuffed") {
-                                return crustPrice.stuffed * 1;
-                            } else {
-                                return crustPrice.gluten * 1;
-                            };
-                        };
+        function crustCalcPrice(crust) {
+            if (crust === "crispy") {
+                return crustPrice.crispy * 1;
+            } else if (crust === "stuffed") {
+                return crustPrice.stuffed * 1;
+            } else {
+                return crustPrice.gluten * 1;
+            };
+        };
 
-                        var selectedDeliver = parseInt($('#delivery-option').val());
-                        var deliveryPrices = [250, 0]
-                        var crust = $('#crust').val();
-                        var toppings = $('#toppings').val();
-                        var size = parseInt($('#size').val());
-                        var pizzaPrice = (sizePrice, toppingPrice, crustPrice);
-                        var inputtedQuantity = parseInt($('#quantity').val());
-                        var deliveryPrice = deliveryPrices[selectedDeliver - 1];
+        var selectedDeliver = parseInt($('#delivery-option').val());
+        var deliveryPrices = [250, 0]
+        var crust = $('#crust').val();
+        var toppings = $('#toppings').val();
+        var size = parseInt($('#size').val());
+        var pizzaPrice = (sizePrice, toppingPrice, crustPrice);
+        var inputtedQuantity = parseInt($('#quantity').val());
+        var deliveryPrice = deliveryPrices[selectedDeliver - 1];
 
-                        var newDetails = new Details(size, crust, toppings, inputtedQuantity);
-                        var newTotal = new Total(sizePrice, crustPrice, toppingPrice, inputtedQuantity, deliveryPrice);
-                        var newBill = newTotal.finalTotal();
+        var newDetails = new Details(size, crust, toppings, inputtedQuantity);
+        var newTotal = new Total(sizePrice, crustPrice, toppingPrice, inputtedQuantity, deliveryPrice);
+        var newBill = newTotal.finalTotal();
 
 
-                        var totalQuantity = parseInt($("total").val());
+        var totalQuantity = parseInt($("total").val());
 
-                        function calcTotal() {
-                            var priceOnePizza =
-                                sizeCalcPrice(sizePrice()) +
-                                crustCalcPrice(crustPrice()) +
-                                toppingsCalcPrice(toppingPrice());
-                            return priceOnePizza;
-                        }
-                        $("#order").on("click", function() {
+        function calcTotal() {
+            var priceOnePizza =
+                sizeCalcPrice(sizePrice()) +
+                crustCalcPrice(crustPrice()) +
+                toppingsCalcPrice(toppingPrice());
+            return priceOnePizza;
+        }
+        $("#order").on("click", function() {
 
-                        });
+        });
+
+        var size = $("#size").find(":selected").val();
+        alert("your order is " + ", " + size + ' ,' + newDetails.selectedToppings + ' and ' + newDetails.selectedCrust + ' ' + "pizza" + ". click ok to view your bill");
+        var deliver = confirm(
+            "Would you like your pizza to be delivered to you? transport cost ksh 250."
+        );
+        if (deliver) {
+            var place = prompt("Enter your location");
+            $("#place").text(place);
+            $("#success").show();
+        } else {
+            $("#no-delivery").show();
+        }
+
+        alert('total to be paid is' + ' ' + "1450");
+        document.getElementById('allme').reset();
+
+    });
+});
