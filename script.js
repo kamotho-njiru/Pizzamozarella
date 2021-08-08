ee7571b6ee4e0f877d634e3e040cbcdb3a5ca221
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-08 14:46:28 +0300

    add function total

---
+++ script.js
@@ -0,0 +1,6 @@
+function Details(size, crust, toppings, quantity) {
+    this.selectedSize = size;
+    this.selectedCrust = crust;
+    this.selectedToppings = toppings;
+    this.selectedQuantity = quantity;
+}
